68096caa718c4307319cdc1aeb280b100afbdca8
Joao Victor <jov9051@outlook.com>
2022-04-07 14:41:11 -0300

    feat: handleFile importado para userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import multer from 'multer';
 
+import { handleFile } from '../controllers/userController.js';
+
 const upload = multer();
 
 const userRouter = express.Router();
