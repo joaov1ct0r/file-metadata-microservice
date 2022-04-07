05b187f3b2d993223ee7ff280ed21c98612184b3
Joao Victor <jov9051@outlook.com>
2022-04-07 14:37:45 -0300

    feat: handleFile criado em userController.js

---
+++ src/controllers/userController.js
@@ -0,0 +1,10 @@
+const handleFile = (req, res) => {
+    if (!req.file) return res.json({ error: 'Arquivo não encontrado!' });
+    else {
+        res.json({
+            name: req.file.originalname,
+            type: req.file.mimetype,
+            size: req.file.size
+        });
+    }
+};
